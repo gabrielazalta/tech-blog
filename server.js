07672a24ffb0f4947fb4ce4ca002cd0424f52739
Gabriela Zalta <gzalta96@gmail.com>
2021-02-07 17:58:19 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.DB_SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -34,3 +34,4 @@ app.use(require('./controllers/'));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`API server now on port ${PORT}!`));
 });
+
